6787d007c6d8b88f4728383c24a8d37da015b0cc
Shubham Kumar <shubhamaother@gmail.com>
2021-10-07 14:24:34 +0530

    07102021

--- app.js
+++ app.js
@@ -4,25 +4,8 @@ const port = 3003;
 const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
-const mongoose = require("./database")
-const session = require("express-session")
-
-// mongoose.connect("mongodb+srv://admin:Sk@961969@twitterclonecluster.a8lzi.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority")
-// .then(()=>{
-    // console.log("Database connection successfull");
-// })
-// .catch((err)=>{
-    // console.log("database not connection successfull"+ err);
-// })
-
-// mongoose.connect("mongodb://shubham:20lZu5uATfqqr7uc@clusterog-shard-00-00.h0ilj.mongodb.net:27017,clusterog-shard-00-01.h0ilj.mongodb.net:27017,clusterog-shard-00-02.h0ilj.mongodb.net:27017/shubham?ssl=true&replicaSet=atlas-yoi750-shard-0&authSource=admin&retryWrites=true&w=majority")
-// .then(()=>{
-//     console.log("DB DONE")
-// })
-// .catch((err)=>{
-//     console.log("DB NOT DONE" + err);
-// })
-
+const mongoose = require("./database");
+const session = require("express-session");
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
@@ -33,26 +16,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: "bbq chips",
     resave: true,
-    saveUninitialized: false,
-    // cookie: { secure: true }
-  }))
+    saveUninitialized: false
+}))
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoute');
-const logoutRoute = require('./routes/logout')
+
+// Api routes
+const postsApiRoute = require('./routes/api/posts');
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
-app.use("/logout", logoutRoute);
+
+app.use("/api/posts", postsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
         pageTitle: "Home",
-        userLoggedIn : req.session.user
+        userLoggedIn: req.session.user
     }
 
     res.status(200).render("home", payload);
---
+++ public/js/common.js
@@ -0,0 +1,36 @@
+$("#postTextarea").keyup(event => {
+    var textbox = $(event.target);
+    var value = textbox.val().trim();
+    
+    var submitButton = $("#submitPostButton");
+
+    if(submitButton.length == 0) return alert("No submit button found");
+
+    if (value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+
+    submitButton.prop("disabled", false);
+})
+
+$("#submitPostButton").click(() => {
+    var button = $(event.target);
+    var textbox = $("#postTextarea");
+
+    var data = {
+        content: textbox.val()
+    }
+
+    $.post("/api/posts", data, postData => {
+        
+        var html = createPostHtml(postData);
+        $(".postsContainer").prepend(html);
+        textbox.val("");
+        button.prop("disabled", true);
+    })
+})
+
+function createPostHtml(postData) {
+    return postData.content;
+}
