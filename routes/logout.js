3d7f84518d42a5d6f8425cc9647ba63bd22beed4
Shubham Kumar <shubhamaother@gmail.com>
2021-11-03 13:11:48 +0530

    03112021

--- routes/logout.js
+++ routes/logout.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res, next) => {
     if(req.session){
+        // Once Logged Out destroy the session and redirect the user to the loginPage
         req.session.destroy(()=>{
             res.redirect("/login")
         })
